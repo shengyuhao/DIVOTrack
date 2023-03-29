8fb326b5be0f9807ddb79bd1ef0e2e1fac91bcb4
hank0626 <hank010626@163.com>
2023-03-30 01:38:27 +0800

    update format

--- CrossMOT/experiments/test.sh
+++ CrossMOT/experiments/test.sh
@@ -1,14 +1,15 @@
 # EPFL
-python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_epfl --conf_thres 0.5 
+# python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_epfl --conf_thres 0.5 
 
 # DIVOTrack
-python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_divo --conf_thres 0.5 
+python trackm.py mot --exp_name DIVOTrack --load_model ${model_path} --test_divo --conf_thres 0.5 
 
 # MvMHAT
-python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_mvmhat --conf_thres 0.5 
+# python trackm.py mot --exp_name MvMHAT --load_model ${model_path} --test_mvmhat --conf_thres 0.5 
 
 # CAMPUS
-python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_mvmhat_campus --conf_thres 0.5 
+# python trackm.py mot --exp_name CAMPUS --load_model ${model_path} --test_mvmhat_campus --conf_thres 0.5 
 
 # Wildtrack
-python trackm.py mot --exp_name EPFL --load_model ${model_path} --test_wildtrack --conf_thres 0.5 
+# python trackm.py mot --exp_name Wildtrack --load_model ${model_path} --test_wildtrack --conf_thres 0.5
+ 
