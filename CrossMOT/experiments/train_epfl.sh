8a920d63bec205ea8a7649bc99c942815e0b2bb3
Peiyuan Liu <77595899+Hank0626@users.noreply.github.com>
2023-03-30 12:01:48 +0800

    Update train_epfl.sh

--- CrossMOT/experiments/train_epfl.sh
+++ CrossMOT/experiments/train_epfl.sh
@@ -1,2 +1,3 @@
 # EPFL
+cd src
 python train.py mot --exp_id DIVOTrack --data_cfg '../src/lib/cfg/EPFL.json' --load_model "../models/fairmot_dla34.pth" --gpus 0,1,2,3 --batch_size 32 --num_epochs 30 --zero_start
