703e128fda07cf46197b46c5f576387d76ee8c74
wlf-darkmatter <62014693+wlf-darkmatter@users.noreply.github.com>
2023-04-07 23:57:53 +0800

    update train cmd (#4)

--- CrossMOT/experiments/train_divo.sh
+++ CrossMOT/experiments/train_divo.sh
@@ -1,3 +1,3 @@
 # DIVOTrack
 cd src
-python train.py mot --exp_id EPFL --data_cfg '../src/lib/cfg/divo.json' --load_model "../models/fairmot_dla34.pth" --gpus 0,1,2,3 --batch_size 32 --num_epochs 30
+python train.py mot --exp_id DIVOTrack --data_cfg '../src/lib/cfg/divo.json' --load_model "../models/fairmot_dla34.pth" --gpus 0,1,2,3 --batch_size 32 --num_epochs 30
